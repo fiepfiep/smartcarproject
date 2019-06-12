cf94afa5cea6dcb1f4c1325012fdf16a1cb84986
Bert Marres <bert.marres@hotmail.com>
2019-06-12 17:30:41 +0200

    First line following robotcar code

---
+++ code/BERT/Following_Line/Following_Line.ino
@@ -0,0 +1,221 @@
+/*LIBRARIES*/
+#include <Wire.h> // for LCD
+#include <LiquidCrystal_PCF8574.h>  // for LCD
+
+/*PINS*/
+const int IN1 = 10; //Pins usded for motor driver
+const int IN2 = 7;
+const int IN3 = 8;
+const int IN4 = 9;
+const int ENA = 5;
+const int ENB = 6; //Pins usded for motor driver
+
+const int IR_LEFT = 2; //Pins used for line-detecting IR modules
+const int IR_MIDDLE = 4;
+const int IR_RIGHT = 3;
+
+/*INIT DEVICES*/
+LiquidCrystal_PCF8574 lcd(0x27);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+
+/*OTHERS*/
+bool operating = true; //show when the car is following the line and when it is completely lost
+unsigned long prev_time = 0;
+int ir_l, ir_m, ir_r; // inputs for line tracking
+
+void setup()
+{
+  for (int i = 5; i < 11; i ++) //set motor pins as output
+  {
+    pinMode(i, OUTPUT);
+  }
+
+  for(int i = 2 ; i < 5; i++){ //set line-detecting IR's as input
+    pinMode(i, INPUT);
+  }
+  
+  Serial.begin(9600);
+
+  //LCD setup
+  Wire.begin();
+  Wire.beginTransmission(0x27);
+  lcd.begin(16, 2);
+  lcd.setBacklight(255);
+  lcd.home(); lcd.clear();
+  lcd.blink();
+  lcd.print("Altran Smart Car....");
+  delay(1000);
+}
+
+void loop(){
+
+	
+	while(operating){
+	  	operating = drive_line();
+	}
+
+}
+
+bool drive_line(){ //track a line. Stop if no line found after some time
+  //NOTE: At this point, the line is continuously derivable
+
+  double softTurn_ratio = 0.3; //turn at about 1 carpet tile (altran brussels office floor carpet)
+  double hardTurn_ratio = 0; //turn at about 1/2 carpet tile
+  int speed = 170;
+
+  int line = 1; // defines the color of the line
+  int no_line = 0;
+
+  bool back_on_track; //when car gets lost
+
+
+  //Get detections: 1 -> black detected 0 -> white detected
+  ir_l = digitalRead(IR_LEFT);
+  ir_m = digitalRead(IR_MIDDLE);
+  ir_r = digitalRead(IR_RIGHT);
+
+  //Turn based on measurements
+  if(ir_l == no_line && ir_m == line && ir_r == no_line){
+    // continue straight ahead
+    control_motors(speed, speed);
+    Serial.println("Straight ahead");
+    lcd.home();
+    lcd.clear();
+    lcd.print("Straight ahead");
+  }
+  else if (ir_l == line && ir_m == line && ir_r == line){
+    // Stop
+    control_motors(0, 0);
+    Serial.println("Stop");
+    lcd.home();
+    lcd.clear();
+    lcd.print("Stop, destination found");
+    return false;
+  }
+  else if (ir_l == line && ir_m == line && ir_r == no_line){
+    // soft turn left
+    control_motors(round(softTurn_ratio * speed), speed);
+    Serial.println("Soft turn left");
+    lcd.home();
+    lcd.clear();
+    lcd.print("Soft left");
+  }
+  else if (ir_l == line && ir_m == no_line && ir_r == no_line){
+    // hard turn left
+    control_motors(round(hardTurn_ratio * speed), speed);
+    Serial.println("Hard turn left");
+    lcd.home();
+    lcd.clear();
+    lcd.print("Hard left");
+  }
+  else if (ir_l == no_line && ir_m == line && ir_r == line){
+    // soft turn right
+    control_motors(speed, round(softTurn_ratio * speed));
+    Serial.println("Soft turn right");
+    lcd.home();
+    lcd.clear();
+    lcd.print("Soft right");
+  }
+  else if (ir_l == no_line && ir_m == no_line && ir_r == line){
+    // hard turn right
+    control_motors(speed, round(hardTurn_ratio * speed));
+    Serial.println("Hard turn right");
+    lcd.home();
+    lcd.clear();
+    lcd.print("Hard right");
+  }
+  else if (ir_l == no_line && ir_m == no_line && ir_r == no_line){
+    // car is lost
+    control_motors(0, 0);
+    Serial.println("Backup, car is lost...");
+    lcd.home();
+    lcd.clear();
+    lcd.print("Backup, car lost");
+    delay(2000);
+    lcd.home();
+    lcd.clear();
+    lcd.print("Backing up...");
+    back_on_track = Backup_for_line();
+    if(!back_on_track){
+    	lcd.home();
+    	lcd.clear();
+    	lcd.print("Completely lost...");
+    	Serial.println("Car is completely lost...");
+    	return false;
+    }
+  }
+  else if (ir_l == line && ir_m == no_line && ir_r == line){
+    // corrupt state
+    control_motors(0, 0);
+    Serial.println("ERROR, what is happening, is there a split in the line?");
+    lcd.home();
+    lcd.clear();
+    lcd.print("ERROR split line");
+    return false;
+  }
+
+  return true;
+} 
+
+bool Backup_for_line(){
+	//Go in reverse till line is found, or till timeout
+
+	double softTurn_ratio = 0.3; //turn at about 1 carpet tile (altran brussels office floor carpet)
+	double hardTurn_ratio = 0; //turn at about 1/2 carpet tile
+	int speed = -80;
+
+	int line = 1; // defines the color of the line
+	int no_line = 0;
+
+	bool line_found = false;
+	
+	unsigned long timeout = 10000; //10s
+
+	unsigned long time = millis();
+	while((millis() - time < timeout) && !line_found){
+		//Go backwards
+		control_motors(speed,speed);
+		delay(100);
+		control_motors(0,0);
+
+		//check for line
+		ir_l = digitalRead(IR_LEFT);
+		ir_m = digitalRead(IR_MIDDLE);
+		ir_r = digitalRead(IR_RIGHT);
+
+		if(ir_l == line || ir_m == line || ir_r == line){
+			line_found = true;
+		}
+	}
+
+	return line_found;
+}
+
+void control_motors(int left, int right)
+{
+
+  if (right > 0)
+  {
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    analogWrite(ENA, right);
+  }
+  else
+  {
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    analogWrite(ENA, -right);
+  }
+  if (left > 0)
+  {
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+    analogWrite(ENB, left);
+  }
+
+  else
+  {
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+    analogWrite(ENB, -left);
+  }
+}
---
+++ code/BERT/Motor_Test/Motor_Test.ino
@@ -0,0 +1,58 @@
+/*PINS*/
+const int IN1 = 10; //Pins usded for motor driver
+const int IN2 = 7;
+const int IN3 = 8;
+const int IN4 = 9;
+const int ENA = 5;
+const int ENB = 6; //Pins usded for motor driver
+
+void setup()
+{
+  for (int i = 5; i < 11; i ++) //set motor pins as output
+  {
+    pinMode(i, OUTPUT);
+  }
+  Serial.begin(9600);
+}
+
+void loop(){
+
+  double softTurn_ratio = 0.3; //turn at about 1 carpet tile (altran brussels office)
+  double hardTurn_ratio = 0; //turn at about 1/2 carpet tile
+  float speed = 150;
+
+  //drive circle counter clockwise
+  control_motors(round(softTurn_ratio * speed), speed);
+
+}
+
+void control_motors(float left, float right)
+{
+
+
+  if (right > 0)
+  {
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    analogWrite(ENA, right);
+  }
+  else
+  {
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    analogWrite(ENA, -right);
+  }
+  if (left > 0)
+  {
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+    analogWrite(ENB, left);
+  }
+
+  else
+  {
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+    analogWrite(ENB, -left);
+  }
+}
